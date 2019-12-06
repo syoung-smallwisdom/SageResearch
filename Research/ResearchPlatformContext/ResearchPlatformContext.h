848a831c0054f37754e7176c1dd07dbeb5b359e3
Shannon Young <shannon.young@sagebase.org>
2019-12-05 17:35:59 -0800

    Add platform context framework
    Fix up ResearchPlatformContext

---
+++ Research/ResearchPlatformContext/ResearchPlatformContext.h
@@ -0,0 +1,19 @@
+//
+//  ResearchPlatformContext.h
+//  ResearchPlatformContext
+//
+//  Created by Shannon Young on 12/5/19.
+//  Copyright © 2019 Sage Bionetworks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResearchPlatformContext.
+FOUNDATION_EXPORT double ResearchPlatformContextVersionNumber;
+
+//! Project version string for ResearchPlatformContext.
+FOUNDATION_EXPORT const unsigned char ResearchPlatformContextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResearchPlatformContext/PublicHeader.h>
+
+
